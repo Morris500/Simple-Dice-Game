3fcaff16338c5c677b4b5324dd6246f50be3eb94
morris <akunnemorris@gmail.com>
2024-05-18 02:05:57 +0100

    Dice game

---
+++ index.js
@@ -0,0 +1,31 @@
+/*alert ("hello world");
+var name = prompt ("enter your name");
+*/
+function start() {
+  dice1=Math.floor(Math.random()*6)+1;
+  
+  document.querySelector(".img1").setAttribute("src", "dice-img/faces"+dice1+".png")
+  
+  
+  dice2=Math.floor(Math.random()*6)+1;
+  
+  document.querySelector(".img2").setAttribute("src", "dice-img/faces"+dice2+".png")
+  
+  var heading = document.getElementsByTagName("h1")[0];
+  
+  if (dice1===dice2) { heading.innerHTML=("Draw")
+  
+  } else if (dice1>dice2){heading.innerHTML=("player1 wins")
+  
+  }
+  
+  else {
+    heading.innerHTML=("player2 wins")
+  }
+   
+}
+
+document.querySelector(".btn").addEventListener("click", ()=>{
+start();
+});
+
